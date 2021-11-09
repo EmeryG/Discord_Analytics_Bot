1c9c8f6ce8b989f2ce303f2fc9bc090f5f374e7b
EmeryG <Emerytgriffith@gmail.com>
2021-11-09 16:29:29 -0600

    Add ability to get bot access token via OAuth2.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
-import json
+import json, requests
 
-DISCORD_API = "https://discord.com/api"
+DISCORD_API = "https://discord.com/api/v8"
 BOT_ID = "907693823407312896"
 
 with open('bot_secret.txt', 'r') as file:
     BOT_SECRET = file.read().rstrip()
 
-print(BOT_SECRET)
+def get_token():
+    data = {
+        'grant_type': 'client_credentials',
+        'scope': 'identify connections'
+    }
+    
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    
+    r = requests.post(DISCORD_API + '/oauth2/token', data=data, headers=headers, auth=(BOT_ID, BOT_SECRET))
+    r.raise_for_status()
+    return r.json()
+
+print(get_token())
+    
