d6615ee2ba490164b9673833db7781e0ca388c84
EmeryG <Emerytgriffith@gmail.com>
2021-11-09 19:32:57 -0600

    Get JSON of a guild user list

--- src/app.py
+++ src/app.py
@@ -1,24 +1,23 @@
 import json, requests
 
-DISCORD_API = "https://discord.com/api/v8"
-BOT_ID = "907693823407312896"
+DISCORD_API = 'https://discord.com/api/v8'
+BOT_ID = '907693823407312896'
 
 with open('bot_secret.txt', 'r') as file:
     BOT_SECRET = file.read().rstrip()
+    
+with open('bot_token.txt', 'r') as file:
+    BOT_TOKEN = file.read().rstrip()
 
-def get_token():
-    data = {
-        'grant_type': 'client_credentials',
-        'scope': 'identify connections'
-    }
-    
+def get_users(guild_id):
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'authorization': f'Bot {BOT_TOKEN}'
     }
     
-    r = requests.post(DISCORD_API + '/oauth2/token', data=data, headers=headers, auth=(BOT_ID, BOT_SECRET))
+    r = requests.get(DISCORD_API + f'/guilds/{guild_id}/members', headers=headers)
+
     r.raise_for_status()
+    
     return r.json()
 
-print(get_token())
-    
+print(get_users(819950265552601091))
