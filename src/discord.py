f9a61617ad5f1cf6e6bdfe3b17f51cd908b19139
EmeryG <Emerytgriffith@gmail.com>
2021-11-10 21:00:07 -0600

    Add function to POST basic role analytics for Socionics roles.

--- src/discord.py
+++ src/discord.py
@@ -1,4 +1,5 @@
 import requests
+from requests.api import head
 
 DISCORD_API = 'https://discord.com/api/v8'
 BOT_ID = '907693823407312896'
@@ -15,16 +16,29 @@ class Guild:
     def __init__(self, guild_id):
         self.guild_id = guild_id
     
-    def get_req(self, extension):
-        r = requests.get(DISCORD_API + f'/guilds/{self.guild_id}/{extension}', headers=headers)
+    def post_req(self, extension, data, params={}):
+        r = requests.post(DISCORD_API + extension, headers=headers, data=data, params=params)
+        r.raise_for_status()
+        return r.json()
+    
+    def get_req(self, extension, params={}):
+        r = requests.get(DISCORD_API + extension, headers=headers, params=params)
         r.raise_for_status()
         return r.json()
     
     def get_users(self):
-        return self.get_req('users')
+        return self.get_req(f'/guilds/{self.guild_id}/roles', { 'limit': 1000 })
 
     def get_roles(self):
-        return self.get_req('roles')
+        return self.get_req(f'/guilds/{self.guild_id}/roles')
+    
+    def post_message(self, message):
+        data = {
+            'content': message,
+            'tts': False
+        }
+
+        return self.post_req('/channels/908169250684960778/messages', data)
 
 with open('server_id.txt', 'r') as file:
     DEFAULT_GUILD = Guild(file.read().rstrip())
--- src/schema.py
+++ src/schema.py
@@ -1,5 +1,24 @@
 from discord import DEFAULT_GUILD
 
+specified_roles = {
+    '848352240481337364': 0,
+    '848352377383288842': 0,
+    '848352529384341504': 0,
+    '848362004028850176': 0,
+    '848360647946141736': 0,
+    '848361168342351883': 0,
+    '848361273330368523': 0,
+    '848360732319547413': 0,
+    '848360926369677322': 0,
+    '848361372550430731': 0,
+    '848360831069454336': 0,
+    '848361457866375210': 0,
+    '848361019167735868': 0,
+    '848361536673939456': 0,
+    '848361621239234571': 0,
+    '848361883408269363': 0
+}
+
 def get_user_info():
     user_list = DEFAULT_GUILD.get_users()
     
@@ -19,12 +38,28 @@ def get_user_info():
 def get_role_names():
     role_list = DEFAULT_GUILD.get_roles()
     
-    role_name_map = []
+    role_name_map = {}
     
     for role in role_list:
-        role_name_map.append({
-            'id': role['id'],
-            'name': role['name'],
-        })
+        role_name_map[role['id']] = role['name']
     
     return role_name_map
+
+def send_role_count():
+    users = get_user_info()
+    role_list = get_role_names()
+    
+    for user in users:
+        for role in specified_roles.keys():
+            if role in user['roles']:
+                specified_roles[role] += 1
+    
+    message = []
+    
+    for role in specified_roles.keys():
+        message.append(f'{role_list[role]}: {specified_roles[role]}')
+        
+    DEFAULT_GUILD.post_message('\n'.join(message))
+    
+    
+    
