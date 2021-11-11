a1c4d887dc340aaae61125833f79c6a54bb37101
EmeryG <Emerytgriffith@gmail.com>
2021-11-10 20:06:31 -0600

    Add MySQL database connection

--- src/app.py
+++ src/app.py
@@ -1,3 +1,8 @@
-import json, schema
+import json, schema, mysql.connector
 
+connection = mysql.connector.connect(
+                            user='admin',
+                            password='password',
+                            host='host.docker.internal'
+                                    )
 print(schema.get_role_names())
